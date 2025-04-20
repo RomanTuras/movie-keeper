f6bc982e6259bf4c3b6a56c0d18d371389cd3e6e
Rapt0r <trd.developer@gmail.com>
2025-04-20 20:55:57 +0300

    Added dynamic movie list feature

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import "./style.css";
-import { setupCounter } from "./counter.js";
+let movies = ["Home alone(1994)", "Ghost busters", "Battleship"];
+// let movies = []
+const movieList = document.getElementById("movie-list");
+let movieListContent = `<li class="empty-list-item">Add a movie for your list</li>`;
 
-// document.querySelector('#app').innerHTML = ``
+if (movies.length > 0) {
+  movieListContent = movies
+    .map(
+      (e) => ` <li class="list-item">
+            <span>${e}</span>
+            <button type="button">delete</button>
+          </li>`
+    )
+    .join(" ");
 
-setupCounter(document.querySelector("#counter"));
+  }
+movieList.innerHTML = movieListContent;
