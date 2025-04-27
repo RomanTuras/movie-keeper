97a281636d668a7ce4519e478e227693f0060e21
Rapt0r <trd.developer@gmail.com>
2025-04-27 19:56:04 +0300

    Feature add and remove films

--- src/main.js
+++ src/main.js
@@ -1,17 +1,38 @@
-let movies = ["Home alone(1994)", "Ghost busters", "Battleship"];
-// let movies = []
-const movieList = document.getElementById("movie-list");
-let movieListContent = `<li class="empty-list-item">Add a movie for your list</li>`;
+document.addEventListener("DOMContentLoaded", () => {
+  let movies = ["Home alone(1994)", "Ghost busters", "Battleship"];
+  // let movies = []
+  const movieList = document.getElementById("movie-list");
+  const addFilmBtn = document.getElementById("add-film");
+  const deleteFilmBtn = document.getElementById("delete-film");
+  let movieListContent = `<li class="empty-list-item">Add a movie for your list</li>`;
 
-if (movies.length > 0) {
-  movieListContent = movies
-    .map(
-      (e) => ` <li class="list-item">
-            <span>${e}</span>
-            <button type="button">delete</button>
-          </li>`
-    )
-    .join(" ");
+  window.deleteFilm = (filmName) => {
+    movies = movies.filter((movie) => movie != filmName);
+    renderFilms(movies);
+  };
 
-  }
-movieList.innerHTML = movieListContent;
+  const renderFilms = (movies) => {
+    if (movies.length > 0) {
+      movieListContent = movies
+        .map(
+          (e) => ` <li class="list-item" data-film="${e}">
+        <span>${e}</span>
+        <button onclick="deleteFilm('${e}')" type="button">delete</button>
+        </li>`
+        )
+        .join(" ");
+    }
+    movieList.innerHTML = movieListContent;
+  };
+
+  const addFilm = () => {
+    const filmName = prompt("Input your film name");
+    // console.log(filmName);
+    if (!filmName || filmName.length < 2) return;
+    movies.push(filmName);
+    renderFilms(movies);
+  };
+
+  addFilmBtn.addEventListener("click", addFilm);
+  renderFilms(movies);
+});
